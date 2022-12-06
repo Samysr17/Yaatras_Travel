4f2a6547a5b6115af1feb50d50278cb90f2c79dd
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2022-12-07 00:16:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Text from './Components/Text';
+import Rest from './Components/Rest';
+import Wallpaper from './Components/Wallpaper';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+
+
+
+
+function App() {
+  return (
+  <div>
+     
+    <Navbar />
+    <Wallpaper />
+    <Rest/>
+    <Text/>
+    <Footer/>
+    
+    
+  </div>
+  );
+}
+
+export default App;
